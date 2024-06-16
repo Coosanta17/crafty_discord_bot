4b0704d35c2a24c1eba073abd4d21ec531b14257
Coosanta <coosantamapy@gmail.com>
2024-06-16 17:41:40 +1000

    made the code more readable.

--- src/api/get_stats.js
+++ src/api/get_stats.js
@@ -1,28 +1,29 @@
-import { statsOptions } from "./client.js";
 import axios from "axios";
+
+import { statsOptions } from "./client.js";
 
 export async function getStats() {
     try {
         const response = await axios(statsOptions);
-        // console.log('Getting statistics of server...'); // For debug purposes.
+        //console.log('Getting statistics of server...'); // For debug purposes.
 
-        if (response.data.status === 'ok') {
-            return {
-                status: response.data.status,
-                waitingStart: response.data.data.waiting_start,
-                running: response.data.data.running,
-                memoryUsage: response.data.data.mem,
-                cpuUsage: response.data.data.cpu,
-                playersOnline: response.data.data.online,
-                maxPlayers: response.data.data.max,
-                // response,
-                ...(response.data.data.started !== 'False' && {
-                    startTime: response.data.data.started
-                }),
-            };
-        } else {
+        if (response.data.status !== 'ok') {
             throw new Error('Unexpected response status from method getStats(): ' + response.data.status);
-        }
+        };
+
+        return {
+            status: response.data.status,
+            waitingStart: response.data.data.waiting_start,
+            running: response.data.data.running,
+            memoryUsage: response.data.data.mem,
+            cpuUsage: response.data.data.cpu,
+            playersOnline: response.data.data.online,
+            maxPlayers: response.data.data.max,
+            ...(response.data.data.started !== 'False' && {
+                startTime: response.data.data.started
+            }),
+            //response,
+        };
     } catch (error) {
         console.error('Error getting stats:', error.message);
         if (error.response) {
