59b2381db42e1879fe9fb71b833c294db9020c83
Coosanta <coosantamapy@gmail.com>
2024-06-16 17:41:07 +1000

    Forgot to add imports here.

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,13 @@
 import { discordClient as client } from "./api/client.js";
-import { autoStop, serverStart } from "./api/start_server.js";
+import { serverStart } from "./api/start_server.js";
 
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is online!`);
-    const autoStopInterval = setInterval(autoStop, 300000); // 5 minutes in milliseconds
 });
 
 client.on('messageCreate', async (message) => {
     if (message.content === '>start') {
-        await serverStart(message); // not sure how awaiting will impact it...
+        await serverStart(message);
     }
 });
 
