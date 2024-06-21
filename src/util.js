749f291744891ad01d808223813cc266cd3aa020
Coosanta <coosantamapy@gmail.com>
2024-06-21 21:02:40 +1000

    add object merging thing

--- src/util.js
+++ src/util.js
@@ -40,7 +40,8 @@ export function isIntervalRunning(intervalID) {
     return !!intervals[intervalID]; // False if undefined.
 }
 
-// JSON libraries
+// JSON libraries:
+
 export async function createJsonFile(filePath, content) {
     console.log("Creating configuration file...");
     try {
@@ -55,7 +56,7 @@ export async function parseJsonFile(filePath){
     return JSON.parse(fs.readFile(filePath, "utf-8"));
 }
 
-// Merges newData into existingData
+// Merges newData into existingData, including values.
 export function mergeJson(existingData, newData) {
     for (const key in newData) {
         if (!newData.hasOwnProperty(key)) {
@@ -72,6 +73,45 @@ export function mergeJson(existingData, newData) {
             existingData[key] = newData[key];
         }
     }
+}
+
+function isValidObject(obj, key) {
+    return typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key]);
+}
+
+// Recursively merges objects without overwriting existing values.
+export function mergeObjects(baseObj, newObj) {
+    for (const key in newObj) {
+        if (!newObj.hasOwnProperty(key)) {
+            continue; // Skip inherited properties
+        }
+
+        if (isValidObject(newObj, key)) { // I smell recursion...
+            if (!baseObj.hasOwnProperty(key)) {
+                baseObj[key] = {};
+            }
+            mergeObjects(baseObj[key], newObj[key]);
+            return;
+        }
+
+        if (!baseObj.hasOwnProperty(key)) { 
+            // runs only if key doesn't exist in baseObj.
+            baseObj[key] = newObj[key];
+        }
+
+    }
+}
+
+// Returns true if objects are the same.
+export function compareObjects(object1, object2) {
+    let keys1 = getObjectKeys(object1);
+    let keys2 = getObjectKeys(object2);
+    
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    return keys1.every((key, index) => key === keys2[index]); // Checks if all keys are the same, boolean result.
 }
 
 function getObjectKeys(object, parentKey){
@@ -102,18 +142,6 @@ function getObjectKeys(object, parentKey){
     return keys.sort();
 }
 
-// Returns true if objects are the same.
-export function compareObjects(object1, object2) {
-    let keys1 = getObjectKeys(object1);
-    let keys2 = getObjectKeys(object2);
-    
-    if (keys1.length !== keys2.length) {
-        return false;
-    }
-
-    return keys1.every((key, index) => key === keys2[index]); // Checks if all keys are the same, boolean result.
-}
-
 /*
 const data1={a:1,b:2,c:{x:9,y:10,z:{zz:[1,2,3,4,5,6,7,8,9]}}};
 const data2={a:1,b:2,c:{x:9,y:10,m:12,z:{zz:[1,2,3,4,5,6,7,8,9],yy:[9,8,7,6,5,4,3,2,1]}},d:{f:"Foo",b:"Bar",t:{tt:{ttt:{tttt:7,ttttt:[6,2,7,9,4,2,5,8,9,7,4,2,1,4,7,8,6,4,8,9]}}}}};
@@ -125,11 +153,14 @@ console.log("data1: "+data1Str+"\n");
 console.log("data2: "+data2Str);
 */
 
-const obj1 = {a:1,b:{c:2,d:[3,4]},e:4}, obj2 = {a:10,b:{c:20,d:[30,40]},e:40}, obj3 = {a:1,b:{c:2,d:[3]},e:4};
+let obj1 = {a:1,b:{c:2,d:[3,4]},e:4}, obj2 = {a:10,b:{c:20,d:[30,40]},e:40,f:90}, obj3 = {a:1,b:{c:2,d:[3]},e:4};
 
 console.log(getObjectKeys(obj2)); // [ 'a', 'b', 'b.c', 'b.d', 'b.d[0]', 'b.d[1]', 'e' ]
 console.log(compareObjects(obj1, obj2)); // true
 console.log(compareObjects(obj1, obj3)); // false
-mergeJson(obj1,obj2);
-console.log(JSON.stringify(obj1, null, 4) + "\n" + JSON.stringify(obj2, null, 4) + "\n" + JSON.stringify(obj3, null, 4));
+
+console.log(JSON.stringify(obj1, null, 4) + "\n\n~~~~\n\n" + JSON.stringify(obj2, null, 4)+"\n\n~~");
+mergeObjects(obj1,obj2);
+console.log("~~\n\n"+JSON.stringify(obj1, null, 4) + "\n\n~~~~\n\n" + JSON.stringify(obj2, null, 4));
+
 console.log(getObjectKeys(obj1));
