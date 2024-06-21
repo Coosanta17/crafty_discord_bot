d659ce5827251290879ae7075697882bdd1621a1
Coosanta <coosantamapy@gmail.com>
2024-06-21 20:29:34 +1000

    Added CompareObjects() function to compare objects (duh) Still has some debug stuff.

--- src/util.js
+++ src/util.js
@@ -39,3 +39,97 @@ export function stopInterval(intervalID) {
 export function isIntervalRunning(intervalID) {
     return !!intervals[intervalID]; // False if undefined.
 }
+
+// JSON libraries
+export async function createJsonFile(filePath, content) {
+    console.log("Creating configuration file...");
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(content, null, 4), "utf-8");
+        console.log("Created config file.")
+    } catch (error) {
+        throw new Error('Error creating config file', error);
+    }
+}
+
+export async function parseJsonFile(filePath){
+    return JSON.parse(fs.readFile(filePath, "utf-8"));
+}
+
+// Merges newData into existingData
+export function mergeJson(existingData, newData) {
+    for (const key in newData) {
+        if (!newData.hasOwnProperty(key)) {
+            return; // Exit early on inherited properties.
+        }
+        if (typeof newData[key] === 'object' && !Array.isArray(newData[key])) {
+            if (!existingData.hasOwnProperty(key)) {
+                existingData[key] = {};
+            }
+            mergeJson(existingData[key], newData[key]);
+        } else if (Array.isArray(newData[key])) {
+            existingData[key] = (existingData[key] || []).concat(newData[key]);
+        } else {
+            existingData[key] = newData[key];
+        }
+    }
+}
+
+function getObjectKeys(object, parentKey){
+    let keys = []; 
+
+    if (!object || typeof object !== "object"){
+        return keys; // Returns empty keys if not object.
+    }
+
+    // If object is an array, iterate over items.
+    if (Array.isArray(object)) {
+        object.forEach((item, index) => {
+            let fullkey = `${parentKey}[${index}]`;
+            keys.push(fullkey);
+            keys = keys.concat(getObjectKeys(item, fullkey));
+        })
+        return keys.sort();
+    }
+
+    for (let key in object) {
+        if (!object.hasOwnProperty(key)) {
+            continue; // Skips inherited properties.
+        }
+        let fullKey = parentKey ? `${parentKey}.${key}` : key;
+        keys.push(fullKey);
+        keys = keys.concat(getObjectKeys(object[key], fullKey));
+    }
+    return keys.sort();
+}
+
+// Returns true if objects are the same.
+export function compareObjects(object1, object2) {
+    let keys1 = getObjectKeys(object1);
+    let keys2 = getObjectKeys(object2);
+    
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    return keys1.every((key, index) => key === keys2[index]); // Checks if all keys are the same, boolean result.
+}
+
+/*
+const data1={a:1,b:2,c:{x:9,y:10,z:{zz:[1,2,3,4,5,6,7,8,9]}}};
+const data2={a:1,b:2,c:{x:9,y:10,m:12,z:{zz:[1,2,3,4,5,6,7,8,9],yy:[9,8,7,6,5,4,3,2,1]}},d:{f:"Foo",b:"Bar",t:{tt:{ttt:{tttt:7,ttttt:[6,2,7,9,4,2,5,8,9,7,4,2,1,4,7,8,6,4,8,9]}}}}};
+
+mergeJson(data1, data2);
+let data1Str = JSON.stringify(data1, null, 4)
+let data2Str = JSON.stringify(data2, null, 4)
+console.log("data1: "+data1Str+"\n");
+console.log("data2: "+data2Str);
+*/
+
+const obj1 = {a:1,b:{c:2,d:[3,4]},e:4}, obj2 = {a:10,b:{c:20,d:[30,40]},e:40}, obj3 = {a:1,b:{c:2,d:[3]},e:4};
+
+console.log(getObjectKeys(obj2)); // [ 'a', 'b', 'b.c', 'b.d', 'b.d[0]', 'b.d[1]', 'e' ]
+console.log(compareObjects(obj1, obj2)); // true
+console.log(compareObjects(obj1, obj3)); // false
+mergeJson(obj1,obj2);
+console.log(JSON.stringify(obj1, null, 4) + "\n" + JSON.stringify(obj2, null, 4) + "\n" + JSON.stringify(obj3, null, 4));
+console.log(getObjectKeys(obj1));
