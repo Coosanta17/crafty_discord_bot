8059cede9442b4497d1c2b9a9e0902c953a5a54a
Coosanta <coosantamapy@gmail.com>
2024-07-06 17:19:42 +1000

    Fix typo

---
+++ src/commands/functions/help_response.js
@@ -0,0 +1,16 @@
+import { EmbedBuilder } from "discord.js";
+
+export function help(prefix) {
+    const helpEmbed = new EmbedBuilder()
+        .setColor(0x969696)
+        .setTitle("Help")
+        .setDescription("Available commands:")
+        .addFields(
+            { name: "START", value: `*Starts the server.*\nUsage: \`${prefix}start\`` },
+            { name: "HELP", value: `*Shows the available commands.*\nUsage: \`${prefix}help\`` },
+            { name: "That"s all for now.", value: "*New features will be added soon!*" }
+        )
+        // .setTimestamp();
+
+    return {embeds: [helpEmbed]};
+}
